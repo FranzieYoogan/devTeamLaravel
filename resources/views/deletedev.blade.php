0b5975ce38aa0c9ba9b8c5a11c9ea0e559ef6c2e
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-03-26 14:04:57 -0300

    scrollbarBug

--- resources/views/deletedev.blade.php
+++ resources/views/deletedev.blade.php
@@ -44,7 +44,7 @@
       </button>
       
       <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-         <div class="menuStyle h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+         <div class="menuStyle h-full px-3 py-4 overflow-y-scroll bg-gray-50 dark:bg-gray-800">
             <ul class="space-y-2 font-medium">
           
                <li>
